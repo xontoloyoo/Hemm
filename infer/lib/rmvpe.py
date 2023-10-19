8562bbb4e163a855b918508caf5e525f5ef9a278
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-10-20 03:16:32 +0700

    Update rmvpe.py

--- infer/lib/rmvpe.py
+++ infer/lib/rmvpe.py
@@ -501,7 +501,7 @@ class RMVPE:
             device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.device = device
         self.mel_extractor = MelSpectrogram(
-            is_half, 128, 16000, 1024, 160, None, 30, 8000
+            is_half, 128, 32000, 1024, 160, None, 30, 8000
         ).to(device)
         if "privateuseone" in str(device):
             import onnxruntime as ort
