e7748b1f984a7f97aba4396f786fdac7ad580cc3
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2023-12-16 21:40:52 +0700

    Update tes3.py

--- tes3.py
+++ tes3.py
@@ -23,7 +23,7 @@ import threading
 import shutil
 import logging
 
-BASE_DIR = Path.cwd()
+#BASE_DIR = Path.cwd()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 weight_uvr5 = os.path.join(BASE_DIR, 'assets/uvr5_weights')
