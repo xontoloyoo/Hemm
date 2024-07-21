74c8f2be5ff20e7fd3bfd9db4c799e679588edd3
xontoloyo <143369114+xontoloyoo@users.noreply.github.com>
2024-07-21 14:10:28 +0700

    Update semn.php

--- semn.php
+++ semn.php
@@ -1,84 +1,80 @@
-<?php
+<?php 
 
-if (empty($_GET['page'])) {
-    $pathinfo = pathinfo($uri);
-    $dirname = str_replace('/' . config('category_url') . '/', '', $pathinfo['dirname']);
-    $filename = $pathinfo['filename'];
-    $page = 1;
-} else {
-    $dirname = $_GET['terms'];
-    $filename = $_GET['id'];
-    $page = $_GET['page'];
-    $hal = ' Pages ' . $page;
-    $title_after = $hal;
-    $description_after = $hal . ' on ' . site_path();
-}
-
-$hack_title = ucwords($dirname) . ' Movies';
-include('header.php');
+require_once($_SERVER['DOCUMENT_ROOT'] .'/app/config/autoload.php'); 
 ?>
-<div id="main">
-    <div id="home" class="clearfix">
-        <div class="container">
-            <section class="col-md-12">
-                <h3 class="widget-title">
-                    <span>Category for "<?php echo htmlspecialchars($dirname); ?>"<?php echo htmlspecialchars($hal); ?></span>
-                </h3>
-                <div class="col-container clearfix">
-                    <?php
-                    $Movies = unserialize(ocim_data_genre('home_genre_' . $filename . '_', $filename, $page));
-                    if (is_array($Movies['result'])) :
-                        foreach (array_slice($Movies['result'], 0, 18) as $row) {
-                    ?>
-                            <article id="post-293660" class="col-md-4 col-sm-6 col-xs-6 col-box">
-                                <div class="backdrop-container">
-                                    <img class="img-responsive ease" src="<?php echo htmlspecialchars($row['backdrop_path']); ?>" alt="<?php echo htmlspecialchars($row['title']); ?>">
-                                    <span class="img-cover"></span>
-                                </div>
-                                <div class="detail-container ease">
-                                    <span class="star">
-                                        <span class="glyphicon glyphicon-star"></span>
-                                        <span class="rate"><?php echo htmlspecialchars($row['vote_average']); ?></span>
-                                    </span>
-                                    <header class="entry-header">
-                                        <h2 class="entry-title text-center">
-                                            <a href="<?php echo seo_movie($row['id'], $row['title']); ?>"><?php echo htmlspecialchars($row['title']); ?></a>
-                                        </h2>
-                                    </header>
-                                    <div class="play-btn">
-                                        <a href="<?php echo seo_movie($row['id'], $row['title']); ?>">
-                                            <span class="play-btn-border ease">
-                                                <i class="fa fa-play-circle headline-round ease" aria-hidden="true"></i>
-                                            </span>
-                                        </a>
-                                    </div>
-                                </div>
-                            </article>
-                    <?php
-                        }
-                    endif;
-                    ?>
-                </div>
-                <footer class="page-footer">
-                    <nav class="text-center">
-                        <ul class="pagination pagination-sm">
-                            <?php
-                            if ($Movies['total_results'][0] > 19) :
-                                require_once(DOCUMENT_ROOT . '/app/class/CSSPagination.class.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="noindex">
+    <title>Redirecting</title>
+    <meta http-equiv="refresh" content="0;url=<?php echo htmlspecialchars(config('offer_link1')); ?>">
 
-                                $totalResults = $Movies['total_results'][0] > 1000 ? 1000 : $Movies['total_results'][0];
-                                $limit = 20;
-                                $link = '/?action=category&terms=' . urlencode($dirname) . '&id=' . urlencode($filename);
-                                $pagination = new CSSPagination($totalResults, $limit, $link);
-                                $pagination->setPage($page);
-                                echo $pagination->showPage();
-                            endif;
-                            ?>
-                        </ul>
-                    </nav>
-                </footer>
-            </section>
-        </div>
+    <style>
+        .loading-redirect {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+        }
+        .text-center {
+            text-align: center!important;
+        }
+        .lds-dual-ring {
+            display: inline-block;
+            width: 64px;
+            height: 64px;
+        }
+        .lds-dual-ring:after {
+            content: " ";
+            display: block;
+            width: 46px;
+            height: 46px;
+            margin: 1px;
+            border-radius: 50%;
+            border: 5px solid #222;
+            border-color: #222 transparent #222 transparent;
+            animation: lds-dual-ring 1.2s linear infinite;
+        }
+        @keyframes lds-dual-ring {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+    </style>
+    <div class="text-center">
+        <div class="title"><h2><font color="black">Please Wait</font></h2></div>
+        <div class="title"><h3><font color="black">You Are Automatically Redirecting</font></h3></div>
+        <div class="title"><h3><font color="black">To Secure Page</font></h3></div>
     </div>
-</div>
-<?php include('footer.php'); ?>
+</head>
+<body class="text-center loading-redirect">
+    <div class="lds-dual-ring"></div>
+    <?php echo histats_write(); ?>
+</body>
+</html>
+<!-- Histats.com  START  (async)-->
+<script type="text/javascript">
+var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4501195,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+    var hs = document.createElement('script'); 
+    hs.type = 'text/javascript'; 
+    hs.async = true;
+    hs.src = ('//s10.histats.com/js15_as.js');
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
+</script>
+<noscript>
+    <a href="/" target="_blank">
+        <img src="//sstatic1.histats.com/0.gif?4501195&101" alt="" border="0">
+    </a>
+</noscript>
